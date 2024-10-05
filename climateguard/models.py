fabcc39053383962dbb610aab40f34979964ef62
Yoann CROUIN <ycrouin@talend.com>
2024-10-05 08:59:48 +0200

    Detect claims in news articles

--- climateguard/models.py
+++ climateguard/models.py
@@ -1,16 +1,27 @@
-
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from pydantic.type_adapter import TypeAdapter
 
 
 class Claim(BaseModel):
-    claim: str
-    context: str
-    analysis: str
-    disinformation_score: str
-    disinformation_category: str
+    quote: str = Field(
+        description="The exact quote from the article corresponding to the claim"
+    )
+    claim: str = Field(description="The claim that potentially needs verification")
+    context: str = Field(
+        description="A reformulation of the context in which this claim was made (maximum 1 paragraph)"
+    )
+    analysis: str = Field(
+        description="Analysis from your expert's perspective on the potential misinformation of this claim based on the context."
+    )
+
 
 class Claims(BaseModel):
-    claims: list[Claim]
+    article_needs_fact_checking: bool = Field(
+        description="Whether the article is misleading and should be fact-checked"
+    )
+    claims: list[Claim] = Field(
+        description="A list of all misleading environmental claims that should be fact-checked"
+    )
 
 
 class Article(BaseModel):
@@ -20,4 +31,8 @@ class Article(BaseModel):
     date: str
     topic: str
     source: str
-    
+
+    @staticmethod
+    def from_json(filepath: str) -> "Article":
+        with open(filepath) as f:
+            return TypeAdapter(list[Article]).validate_json(f.read())
