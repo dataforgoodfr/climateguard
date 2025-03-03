fdec693b451c23a8b2f955e90185dbd83692ca6a
Paul Leclercq <paleclercq@gmail.com>
2025-03-03 18:47:50 +0100

    fix: mediatree url with 2 minutes + wip docker volumes

--- jobs/label_misinformation/tests/test_mediatree.py
+++ jobs/label_misinformation/tests/test_mediatree.py
@@ -7,4 +7,4 @@ def test_get_url_mediatree():
     date_string = "2024-12-12 10:10:10"
     date = datetime.fromisoformat(date_string)
     output = get_url_mediatree(channel="itele", date=date)
-    assert output == "https://keywords.mediatree.fr/player/?fifo=itele&start_cts=1733998210&end_cts=1733999410&position_cts=1733998210"
+    assert output == "https://keywords.mediatree.fr/player/?fifo=itele&start_cts=1733998210&end_cts=1733998330&position_cts=1733998210"
