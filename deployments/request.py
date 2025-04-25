bd469696d5a299656e5de7cf25f08a5c61179e2e
gmguarino <gmguarino1@gmail.com>
2025-04-25 12:11:56 +0200

    feat: deploy with llama.cpp

--- deployments/request.py
+++ deployments/request.py
@@ -1,17 +1,27 @@
-import requests
+import os
 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # OpenAI API standard endpoint
-SERVER_URL = "http://127.0.0.1:8000/v1/chat/completions"
+SERVER_URL = "https://localhost/v1/chat/completions"
 
 request_data = {
     "model": "",
+    "stream": True,
     "messages": [
-        {"role": "system", "content": "You are a helpful assistant on subjects related to climate change."},
-        {"role": "user", "content": "What is climate change?"}
-    ]
+        {
+            "role": "system",
+            "content": "You are a helpful assistant on subjects related to climate change.",
+        },
+        {"role": "user", "content": "What is climate change?"},
+    ],
 }
+headers = {"Authorization": f"Bearer {os.getenv('LLAMA_API_KEY', '12345')}"}
 
 if __name__ == "__main__":
-    response = requests.post(SERVER_URL, json=request_data)    
+    response = requests.post(SERVER_URL, json=request_data, headers=headers, verify=False)
+    response.raise_for_status()
     print(response.json())
