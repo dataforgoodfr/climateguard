b4018af8a23c234c8d9b194f2b6f782fe7b241d8
Giuseppe Guarino <40060221+gmguarino@users.noreply.github.com>
2025-06-05 15:36:05 +0200

    Add fine tuning scripts (#53)
    * add grpo
    
    * bert model fine tuning
    
    * add cards testing
    
    * add sft
    
    * update gitignore
    
    * setup deberta training script
    
    * format train script
    
    * update
    
    * add logging
    
    * remove dev notebook
    
    * fix testing notebook
    
    * rename to deberta
    
    * Move llm training to dedicated folder
    
    * add modernbert
    
    * update scripts

---
+++ climateguard/finetuning/deberta/train.py
@@ -0,0 +1,318 @@
+import argparse
+import json
+import logging
+import os
+import random
+from datetime import datetime
+from typing import Any, Dict, List, Union
+
+import numpy as np
+import pandas as pd
+import wandb
+from datasets import load_dataset
+from datasets.arrow_dataset import Dataset
+from default_training_args import DEFAULT_TRAINING_ARGS
+from dotenv import load_dotenv
+from huggingface_hub import HfFolder
+from llama_index.core.node_parser import SentenceSplitter
+from sklearn.metrics import (
+    f1_score,
+    precision_score,
+    recall_score,
+)
+from transformers import (
+    AutoModelForSequenceClassification,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = predictions.argmax(1)
+    f1 = f1_score(labels, predictions, pos_label=1)
+    recall = recall_score(labels, predictions, pos_label=1)
+    precision = precision_score(labels, predictions, pos_label=1)
+    return {
+        "f1": float(f1) if f1 == 1 else f1,
+        "recall": float(recall) if recall == 1 else recall,
+        "precision": float(precision) if precision == 1 else precision,
+    }
+
+
+def sample_dataset(
+    args: argparse.Namespace, dataset: Union[List[Dict[str, Any]], Dataset]
+):
+    splitter = SentenceSplitter(
+        chunk_size=args.chunk_size,
+        chunk_overlap=args.chunk_overlap,
+    )
+    records = []
+    n_true = 0
+    n_false = 0
+    n_max = args.max_samples_per_class if args.max_samples_per_class else 1e6
+    for record in dataset:
+        if record["misinformation_claims"] and n_true < n_max:
+            for claim in record["misinformation_claims"]:
+                for label in claim["labels"]:
+                    claim_text = claim["text"]
+                    plaintext = (
+                        record["plaintext_whisper"]
+                        .lower()
+                        .replace(".", "")
+                        .replace(",", "")
+                        .replace("?", "")
+                    )
+                    start_claim = record["plaintext_whisper"].find(claim_text)
+                    start_idx = max(start_claim - 100, 0)
+                    end_idx = start_idx + len(claim_text) + 512
+                    text = " ".join(plaintext[start_idx:end_idx].split(" ")[:-1])
+                    chunk = splitter.split_text(text)[0]
+                    records.append({"text": chunk, "label": 1})
+                    n_true += 1
+        elif not record["misinformation"]:
+            if args.chunk and n_false < n_max:
+                chunks = splitter.split_text(
+                    record["plaintext_whisper"]
+                    .lower()
+                    .replace(".", "")
+                    .replace(",", "")
+                )
+                records.append(
+                    {
+                        "text": random.choice(chunks),
+                        "label": int(record["misinformation"]),
+                    }
+                )
+                n_false += 1
+    return records
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="DataForGood/climateguard",
+        help="The HF id of the dataset used for training.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="almanach/camembertav2-base",
+        help="The HF id of the base model to finetune.",
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        default="train",
+        help="Data split to be trained on (default: 'train')",
+    )
+    parser.add_argument(
+        "--train-val-split",
+        type=float,
+        default=0.1,
+        help="Percentage of train set dedicated to validation",
+    )
+    parser.add_argument(
+        "--max-tokens",
+        type=int,
+        default=512,
+        help="Max segments length to train the model on (default: 512).",
+    )
+    parser.add_argument(
+        "--chunk",
+        action="store_true",
+        help="Split texts longer than max tokens into chunks, if flag is present",
+    )
+    parser.add_argument(
+        "--chunk-size",
+        type=int,
+        default=512,
+        help="Chunk size if chunk=True (default: 512).",
+    )
+    parser.add_argument(
+        "--chunk-overlap",
+        type=int,
+        default=0,
+        help="Overlap between chunks if chunk=True (default: 128).",
+    )
+    parser.add_argument(
+        "--training-type",
+        type=str,
+        choices=["full", "out", "linear"],
+        default="out",
+        help=(
+            "Type of finetuning. `full` corresponds to a full finetuning, "
+            "`out` corresponds to training the classifier out layer only and "
+            "`linear` trains both the classification linear layer as well as the linear pooler."
+        ),
+    )
+    parser.add_argument(
+        "--logging-type",
+        type=str,
+        choices=["None", "wandb"],
+        default="wandb",
+        help="Where to log to. Either `None` or `wandb` (for weights and biases)",
+    )
+    parser.add_argument(
+        "--max-samples-per-class",
+        type=int,
+        default=None,
+        help="Number of training samples to use for each class (default: 200).",
+    )
+    parser.add_argument(
+        "--push-to-hub",
+        action="store_true",
+        help="Push training checkpoints to hub if active",
+    )
+    parser.add_argument(
+        "--training-args",
+        type=str,
+        help=(
+            "JSON encoded string containing all the training arguments for the HF Trainer. "
+            "Defaults defined in the default_training_args.py file. Can also be partial "
+            "as the arguments will be updated key by key. "
+            "See https://huggingface.co/docs/transformers/v4.52.3/main_classes/trainer "
+            "for more info"
+        ),
+    )
+
+    args = parser.parse_args()
+    arg_names = "\n".join([f"{key}={value}" for key, value in vars(args).items()])
+    logging.info(f"Starting training with the following arguments: \n{arg_names}")
+
+    load_dotenv()
+    if args.logging_type:
+        wandb.login()
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+    # Load raw dataset
+    train_dataset = load_dataset(args.dataset, split=args.split)
+
+    records = sample_dataset(args, train_dataset)
+    claims_dataset = Dataset.from_pandas(pd.DataFrame.from_records(records))
+    claims_dataset = claims_dataset.class_encode_column("label")
+    split_dataset = claims_dataset.train_test_split(
+        test_size=args.train_val_split, stratify_by_column="label"
+    )
+    logging.info(
+        "Sampled the following distribution of examples from dataset: \n"
+        f"{claims_dataset.to_pandas().label.value_counts()}"
+    )
+
+    # Load Tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(args.model)
+
+    # Tokenize helper function
+    def tokenize(batch):
+        return tokenizer(
+            batch["text"],
+            padding="max_length",
+            truncation=True,
+            max_length=args.max_tokens,
+            return_tensors="pt",
+        )
+
+    # Tokenize dataset
+    tokenized_dataset = split_dataset.map(
+        tokenize, batched=True, remove_columns=["text"]
+    )
+
+    # Prepare model labels - useful for inference
+    labels = list(set(tokenized_dataset["train"]["label"]))
+    num_labels = len(labels)
+    label2id, id2label = dict(), dict()
+    for i, label in enumerate(labels):
+        label2id[label] = str(i)
+        id2label[str(i)] = label
+
+    # Download the model from huggingface.co/models
+    model = AutoModelForSequenceClassification.from_pretrained(
+        args.model, num_labels=num_labels
+    )
+
+    # set all layers to not trainable
+    if args.training_type != "full":
+        for parameter in model.parameters():
+            parameter.requires_grad = False
+
+    if args.training_type == "out":
+        # Set linear classifier to trainable
+        for parameter in model.classifier.parameters():
+            parameter.requires_grad = True
+
+    if args.training_type == "linear":
+        for parameter in model.pooler.dense.parameters():
+            parameter.requires_grad = True
+
+    requires_grad_params = 0
+    total_params = 0
+    for parameter in model.parameters():
+        total_params += parameter.nelement()
+        if parameter.requires_grad:
+            requires_grad_params += parameter.nelement()
+    logging.info(
+        f"Training {round(requires_grad_params/1e6)}M parameters out of {round(total_params/1e6)}M"
+    )
+
+    training_args = DEFAULT_TRAINING_ARGS
+    if args.push_to_hub:
+        hub_params = dict(
+            push_to_hub=args.push_to_hub,
+            hub_strategy="every_save",
+            hub_token=HfFolder.get_token(),
+        )
+        training_args.update(hub_params)
+    if args.training_args:
+        try:
+            training_args.update(json.loads(args.training_args))
+        except json.JSONDecodeError as e:
+            logging.fatal(f"Could not load training arguments from string {args.training_args}")
+            raise e
+    else:
+        logging.warning(
+            "No training arguments have been passed. Defaults will be applied. "
+            "See `default_training_args.py` for more info. To pass customised training "
+            "arguments, use the `--training-args` flag."
+        )
+
+    # Metric helper method
+    # Define training args
+    training_args = TrainingArguments(
+        output_dir="runs/"
+        + args.model.split("/")[-1]
+        + "-"
+        + args.dataset.split("/")[-1],
+        run_name=args.model.split("/")[-1]
+        + "-"
+        + args.dataset.split("/")[-1]
+        + datetime.now().strftime("%d-%m-%Y_%H-%M-%S"),
+        report_to=args.logging_type,
+        **training_args,
+    )
+
+    # Create a Trainer instance
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset["train"],
+        eval_dataset=tokenized_dataset["test"],
+        compute_metrics=compute_metrics,
+    )
+    trainer.train()
+
+    model.save_pretrained(
+        "models/" + args.model.split("/")[-1] + "-" + args.dataset.split("/")[-1]
+    )
+    tokenizer.save_pretrained(
+        "models/" + args.model.split("/")[-1] + "-" + args.dataset.split("/")[-1]
+    )
+
+    model.push_to_hub(args.model.split("/")[-1] + "-" + args.dataset.split("/")[-1])
+    tokenizer.push_to_hub(args.model.split("/")[-1] + "-" + args.dataset.split("/")[-1])
---
+++ climateguard/finetuning/llms/sft/test.py
@@ -0,0 +1,131 @@
+import argparse
+import json
+import re
+import sys
+
+import pandas as pd
+from datasets import load_dataset
+from mlx_lm import generate, load
+from mlx_lm.sample_utils import make_logits_processors, make_sampler
+from sklearn.metrics import classification_report
+from tqdm import tqdm
+sys.path.append("./..")
+
+from prompts import prompt_chat
+
+def remove_thinking(text, thinking_token):
+    if thinking_token:
+        # Remove thinking section
+        start_thought = text.find(f"<{thinking_token}>")
+        end_thought = text.find(f"</{thinking_token}>")
+        if end_thought > start_thought:
+            text = text[end_thought + len(f"</{thinking_token}>") :]
+    return text
+
+def parse_xml(text, thinking_token=None):
+    pattern = r'misinformation>(.*?)</misinformation>'
+    text = remove_thinking(text, thinking_token)
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        misinformation = match.group(1)
+        return {"misinformation": misinformation.strip()}
+    print("No XML match found")
+    print(text)
+    return {"misinformation": False}
+
+def parse_json(text, thinking_token=None):
+    pattern = r"\{.*?\}"  # Matches JSON-like objects
+
+    text = remove_thinking(text, thinking_token)
+
+    match = re.search(pattern, text, re.DOTALL)
+    if match:
+        json_str = match.group(0)
+        try:
+            json_obj = json.loads(json_str)  # Properly parse the JSON
+        except json.JSONDecodeError:
+            json_str = json_str.removeprefix("{").removesuffix("}")
+            json_obj = {}
+            segments = json_str.split(":")
+            json_obj[segments[0].strip().replace('"', "").replace("'", "")] = segments[1].strip().replace('"', "").replace("'", "")
+        finally:
+            if list(json_obj.keys())[0] == "misinformation":
+                return json_obj
+            else:
+                print("Cannot parse JSON")
+                return {"misinformation": False}
+    print("No JSON match found")
+
+
+def parse_bool(response):
+    if isinstance(response, bool) or isinstance(response, int):
+        return int(response)
+    elif isinstance(response, str):
+        return int(response.lower() == "true")
+    print("Cannot parse bool")
+    return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", type=str, default="DataForGood/climateguard")
+    parser.add_argument("-s", "--data-split", type=str, default="test")
+    parser.add_argument(
+        "-m", "--model", type=str, default="mlx-community/Qwen3-0.6B-bf16"
+    )
+    parser.add_argument("-a", "--adapters", type=str, default="adapters")
+    parser.add_argument("-t", "--temp", type=float, default=0.1)
+    parser.add_argument("--max-tokens", type=int, default=512)
+    parser.add_argument("--repetition-penalty", type=float, default=0                                                                                                               )
+    parser.add_argument("--thinking-token", type=str, default=None)
+    parser.add_argument("--return-type", type=str, choices=["json", "xml"], default="json")
+    args = parser.parse_args()
+    print("Found following args")
+    print(str(args))
+
+    parse_function = {
+        "json": parse_json,
+        "xml": parse_xml,
+    }
+
+    load_args = dict(path_or_hf_repo=args.model)
+    if args.adapters != "None":
+        load_args.update(dict(adapter_path=args.adapters))
+    model_lora, tokenizer_lora = load(**load_args)
+
+    dataset = load_dataset(args.dataset, split=args.data_split)
+
+    labels = []
+    predictions = []
+    for record in tqdm(dataset):
+        messages = [
+            {"role": "user", "content": prompt_chat + f"```{record['plaintext']}```"}
+        ]
+
+        prompt = tokenizer_lora.apply_chat_template(
+            messages, add_generation_prompt=True, enable_thinking=args.thinking_token is not None
+        )
+        sampler = make_sampler(temp=args.temp)
+        logits_processors = make_logits_processors(repetition_penalty=args.repetition_penalty)
+        response = generate(
+            model_lora,
+            tokenizer_lora,
+            prompt=prompt,
+            max_tokens=args.max_tokens,
+            verbose=False,
+            sampler=sampler,
+            # logits_processors=logits_processors,
+        )
+        prediction = parse_function[args.return_type](response, thinking_token=args.thinking_token)
+        predictions.append(
+            parse_bool(prediction["misinformation"])
+            if prediction
+            else 0
+        )
+        labels.append(int(record["misinformation"]))
+
+    print(classification_report(labels, predictions))
+    print(pd.Series(predictions).value_counts())
+
+
+
