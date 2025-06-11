e1a97e3c907af20cbb17cbf9819d7d60e3d17061
gmguarino <gmguarino1@gmail.com>
2025-06-11 16:21:17 +0200

    add bert pipeline

--- jobs/label_misinformation/app/pipeline.py
+++ jobs/label_misinformation/app/pipeline.py
@@ -1,14 +1,21 @@
+import logging
+import os
+import re
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-import logging
 from typing import List, Optional
-from secret_utils import get_secret_docker
-import os
-import openai
-import re
-from prompts import DisinformationPrompt
 
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+import pandas as pd
+import openai
+from llama_index.core.node_parser import SentenceSplitter
+from prompts import DisinformationPrompt
+from secret_utils import get_secret_docker
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from torch import nn
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 @dataclass
@@ -22,6 +29,8 @@ class PipelineInput:
     source: Optional[str] = None
     # date and time of the original emission
     date: Optional[str] = None
+    # Keywords ID
+    id: Optional[str] = None
 
 
 @dataclass
@@ -32,11 +41,20 @@ class PipelineOutput:
     reason: str = ""
     # suggestion of other metadata that could be added
     cards_category: Optional[str] = None
+    # probability of generation
+    probability: Optional[float] = None
+    # Keywords ID
+    id: Optional[str] = None
 
 
 class Pipeline(ABC):
     @abstractmethod
     def process(self, input_data: PipelineInput) -> PipelineOutput:
+        """Process input data and return an integer classification."""
+        pass
+
+    @abstractmethod
+    def batch_process(self, input_data: List[PipelineInput]) -> List[PipelineOutput]:
         """Process input data and return an integer classification."""
         pass
 
@@ -74,7 +92,9 @@ def parse_response(response: str) -> PipelineOutput:
 
 
 class SinglePromptPipeline(Pipeline):
-    def __init__(self, model_name: str, api_key: str, prompt: DisinformationPrompt) -> None:
+    def __init__(
+        self, model_name: str, api_key: str, prompt: DisinformationPrompt
+    ) -> None:
         openai_key = get_secret_docker("OPENAI_API_KEY")
         openai.api_key = openai_key
         os.environ["OPENAI_API_KEY"] = openai_key
@@ -83,7 +103,9 @@ class SinglePromptPipeline(Pipeline):
         self._system_prompt = prompt.prompt
         self.prompt_version = prompt.version
         self.version = f"{model_name}/{prompt.version}"
-        self._steps = [f"Single Open AI prompt with {self._model} - prompt version: {prompt.version} - prompt text: {self._system_prompt}"]
+        self._steps = [
+            f"Single Open AI prompt with {self._model} - prompt version: {prompt.version} - prompt text: {self._system_prompt}"
+        ]
 
     def process(self, input_data: PipelineInput) -> int:
         prompt = self._system_prompt + f" '''{input_data.transcript}'''"
@@ -110,3 +132,71 @@ class SinglePromptPipeline(Pipeline):
         for step in self._steps:
             logging.info(step)
         return self._steps
+
+
+class BertPipeline(Pipeline):
+    def __init__(
+        self,
+        model_name: str,
+        tokenizer_name: str = None,
+        chunk_size: int = 512,
+        chunk_overlap: int = 256,
+        batch_size: int = 32,
+    ):
+        self._model = model_name
+        self.model = AutoModelForSequenceClassification.from_pretrained(model_name)
+        for parameter in self.model.parameters():
+            parameter.requires_grad = False
+        if tokenizer_name is None:
+            tokenizer_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
+        self.splitter = SentenceSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+        )
+
+        self._steps = [
+            (
+                f"BERT Pipeline using model: {self._model} - ",
+                f"chunk size: {chunk_size} - prompt text: {chunk_overlap}",
+            )
+        ]
+
+    def batch_process(self, input_data):
+        ids = []
+        texts = []
+        for idx, data_record in enumerate(input_data):
+            chunks = self.splitter.split_text(data_record.transcript)
+            for chunk in chunks:
+                _id = data_record.id if data_record.id else idx
+                ids.append(_id)
+                texts.append(chunks)
+
+        inputs = self.tokenizer(
+            texts["text"],
+            padding="max_length",
+            truncation=True,
+            max_length=self.chunk_size,
+            return_tensors="pt",
+        )
+        outputs = self.model(
+            input_ids=inputs["input_ids"],
+            attention_mask=inputs["attention_mask"],
+            seq_len=self.chunk_size,
+            batch_size=self.batch_size,
+        )
+        results_df = pd.DataFrame(
+            {
+                "id": ids,
+                "prediction": outputs.logits.numpy().argmax(1),
+                "probability": nn.functional.softmax(outputs.logits, dim=-1)
+                .numpy()
+                .max(1),
+            }
+        )
+        results_df = results_df.groupby(["id"]).agg("max").reset_index()
+        return [
+            PipelineOutput(id=row["id"], score=row["prediction"], probability=row["probability"])
+            for idx, row in results_df.iterrows()
+        ]
