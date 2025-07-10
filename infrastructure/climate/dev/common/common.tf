98069a294bc429c240e5fa8f9e037c718f2a4b18
gmguarino <gmguarino1@gmail.com>
2025-07-10 11:25:21 +0200

    split out backend

--- infrastructure/climate/dev/common/common.tf
+++ infrastructure/climate/dev/common/common.tf
@@ -1,22 +1,4 @@
-terraform {
 
-  required_providers {
-    scaleway = {
-      source  = "scaleway/scaleway"
-      version = "~> 2.57"
-    }
-  }
-  backend "s3" {
-    bucket                      = "${var.subject}-safeguards-terraform"
-    key                         = "${var.environment}/common/terraform.tfstate"
-    endpoint                    = "https://s3.fr-par.scw.cloud"
-    region                      = "fr-par"
-    use_lockfile                = true
-    skip_credentials_validation = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-  }
-}
 
 
 module "common" {
