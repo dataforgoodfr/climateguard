1be158230ab5f4e296bff460f56f37037c6bb16f
gmguarino <gmguarino1@gmail.com>
2025-07-10 11:15:19 +0200

    add common module

---
+++ infrastructure/modules/common/main.tf
@@ -0,0 +1,12 @@
+provider "scaleway" {
+  alias      = "project"
+  region     = "fr-par"
+  project_id = var.project_id
+  access_key = var.project_access_key_id
+  secret_key = var.project_secret_access_key
+}
+
+data "scaleway_account_project" "project" {
+  provider = scaleway.project
+  name     = "${var.subject}-safeguards-${var.environment}"
+}
