a0f650d798e8e0d9d30c86bca5ac3b9fa93e6aec
gmguarino <gmguarino1@gmail.com>
2025-07-28 15:52:52 +0200

    add environments command to tofu wrapper

--- infrastructure/bin/tofu_wrapper.sh
+++ infrastructure/bin/tofu_wrapper.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 tofu_cmd() {
-    if [ "$3" == "common" ]; then
+    if [ "$1" == "environments" ]; then
+        source .env && source live/$1/.env.secrets && tofu -chdir=live/$1
+    elif [ "$3" == "common" ]; then
         source .env && source live/$1/$2/$3/.env.secrets && tofu -chdir=live/$1/$2/$3 $4
     else
         source .env && source live/$1/$2/$3/.env.secrets && source live/$1/$2/common/.env.secrets && tofu -chdir=live/$1/$2/$3 $4
