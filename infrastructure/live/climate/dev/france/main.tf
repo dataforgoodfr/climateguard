5cc27f02d1d546944fea15a102d9819928052835
gmguarino <gmguarino1@gmail.com>
2025-07-29 11:11:27 +0200

    go back to variables in main definition as theyu are needed in backend

--- infrastructure/live/climate/dev/france/main.tf
+++ infrastructure/live/climate/dev/france/main.tf
@@ -1,9 +1,9 @@
 module "france" {
   source = "../../../../modules/country"
   # variables
-  subject                    = "climate"
-  environment                = "dev"
-  country                    = "france"
+  subject                    = var.subject
+  environment                = var.environment
+  country                    = var.country
   job_cron_schedule          = "5 6 * * *"
   job_cron_schedule_timezone = "Europe/Paris"
   # Defined as secrets
