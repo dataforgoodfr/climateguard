98759e52bf0fa8b6cd5d765196ef17463a67286a
gmguarino <gmguarino1@gmail.com>
2025-10-17 15:33:40 +0200

    add server setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This script is used to install the environment on a linux computer or server
+# Should work on macOS as well. 
+
+set -e
+
+if ! command -v uv &> /dev/null
+then
+    echo "Installing uv package manager"
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    source ~/.bashrc
+fi
+
+uv sync
+source ./.venv/bin/activate
