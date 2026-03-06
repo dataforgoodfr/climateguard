4bb31b56ce4b738f8a252c3308bee3fdd85483a6
Giuseppe Guarino <40060221+gmguarino@users.noreply.github.com>
2026-03-06 16:52:16 +0100

    Deploy germany env (#101)
    * add germany dev environment
    
    * add germany prod infra
    
    * remov germany suffix from env variables

---
+++ infrastructure/live/climate/prod/germany/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    scaleway = {
+      source  = "scaleway/scaleway"
+      version = "~> 2.57"
+    }
+  }
+  backend "s3" {
+    bucket                      = "${var.subject}-safeguards-terraform"
+    key                         = "${var.country}/${var.environment}/terraform.tfstate"
+    endpoint                    = "https://s3.fr-par.scw.cloud"
+    region                      = "fr-par"
+    use_lockfile                = true
+    skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+  }
+}
