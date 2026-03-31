4ce9016b85cab35a815b963c35b402ef83f3b6c1
gmguarino <gmguarino1@gmail.com>
2026-03-31 16:10:03 +0200

    remove pretty print

--- jobs/get_audio_video/get_audio_video.py
+++ jobs/get_audio_video/get_audio_video.py
@@ -206,20 +206,6 @@ def upload_to_drive(service, folder_id: str, filename: str, content: bytes) -> s
     # Get share link
     return f"https://drive.google.com/file/d/{file['id']}/view?usp=sharing"
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-    ))
 
 def update_labelstudio_task(task_id: int, url_mediatree: str) -> None:
     """Update a Labelstudio task with the new URL."""
